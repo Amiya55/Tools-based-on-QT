66f702777d040ea2bc22153c5d6bedecb2d13e3b
Amiya55 <desf95599@163.com>
2024-09-29 21:07:00 +0800

    新建仓库

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Tools_FILE D:/Codeing/Program/QT_Programs/Tools/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Tools.exe)
+set(__QT_DEPLOY_TARGET_Tools_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Tools_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
